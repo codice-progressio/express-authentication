4c6d598d15cd772b7310c5ed181bbeeba0d2005a
Rafael Ramirez <rafa.yael@gmail.com>
2021-04-24 19:52:36 -0500

    Agregado comentarios en app.js de ejemplo

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT ?? 3000
 
+//Llamamos la libreria.
 const codice_security = require("./index.js")
-
+// Definimos el modo debugs para este demo
 codice_security.configuraciones.debug = true
+// Usamos la configuracion por defecto de cors, pero
+// siempre la podemos sobreescribir. (TODO: Completar todas las opciones)
 codice_security.configuraciones.cors.origin = process.env.ORIGIN
 
+//Llamamos la configuracion de configuracion
 app.use(codice_security.basico())
 
+//Disfrutamos de la vida...
 app.use(express.json({ limit: "5MB" }))
 
 app.get("/", (req, res) => {
