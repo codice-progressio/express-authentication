9727af414acf600f0b9ac65a39a2af10223b1103
Rafael Ramirez <rafa.yael@gmail.com>
2021-05-20 14:35:04 -0500

    Nuevos permisos en archivos de seguridad

--- seguridad/_permisos.seguridad.js
+++ seguridad/_permisos.seguridad.js
@@ -1,4 +1,8 @@
 const permisos = {
+  "usuario:modificar:inhabilitar":"usuario:modificar:inhabilitar",
+  "usuario:modificar:eliminar-permiso":"usuario:modificar:eliminar-permiso",
+  "usuario:modificar:agregar-permiso":"usuario:modificar:agregar-permiso",
+  "usuario:crear":"usuario:crear",
   "administrador":"administrador",
   "login":"login",
   "usuario:leer:todo":"usuario:leer:todo",
--- seguridad/permisos.seguridad.js
+++ seguridad/permisos.seguridad.js
@@ -1,4 +1,8 @@
 const permisos = {
+  "usuario:modificar:inhabilitar":"Deshabilita al usuario ",
+  "usuario:modificar:eliminar-permiso":"Elimina un permiso del usuario",
+  "usuario:modificar:agregar-permiso":"Crea un nuevo permiso al usuario",
+  "usuario:crear":"Crear un nuevo usuario",
   "administrador":"Permisos de administrador",
   "login":"Iniciar sesión en el sistema",
   "usuario:leer:todo":"Leer todos los usuarios registrados en el sistema. ",
